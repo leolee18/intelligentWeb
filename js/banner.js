ceef1dea2852211fe367a68ea1738df5d8a69417
leolee18 <wwwliyuxi@163.com>
2018-12-05 19:23:51 +0800

    更改2

---
+++ js/banner.js
@@ -0,0 +1,2 @@
+$("#banINDWZ").velocity({left: 0,opacity: 1},{duration: 1000,easing: "easeOutSine",delay: 100});
+$("#banINDTP").velocity({top: 60,opacity: 1},{duration: 800,easing: "easeOutSine",delay: 400});
