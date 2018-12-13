449cec7ab63b187224bb2325a74046d9f9e723e5
leolee18 <wwwliyuxi@163.com>
2018-12-13 18:02:45 +0800

    updata

--- js/banner.js
+++ js/banner.js
@@ -1,2 +1,5 @@
 $("#banINDWZ").velocity({left: 0,opacity: 1},{duration: 1000,easing: "easeOutSine",delay: 100});
 $("#banINDTP").velocity({top: 60,opacity: 1},{duration: 800,easing: "easeOutSine",delay: 400});
+
+$("#banProZT").velocity({left: 70,opacity: 1},{duration: 1000,easing: "easeOutSine",delay: 100});
+$("#banProTP").velocity({top: 30,opacity: 1},{duration: 800,easing: "easeOutSine",delay: 400});
